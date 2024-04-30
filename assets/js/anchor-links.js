792567528ff8bb3522e3b48744ef0a233f6e9b2c
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-04-30 11:39:54 -0400

    Update anchor-links.js

--- assets/js/anchor-links.js
+++ assets/js/anchor-links.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const baseUrl = 'https://quarm.guide/'; // Replace 'https://your-site-url.com' with the base URL of your site
     document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach(function(heading) {
+        // Create a span element to contain the icon
+        const iconContainer = document.createElement('span');
+        iconContainer.classList.add('anchor-link-container');
+        
+        // Create the link icon
         const linkIcon = document.createElement('img');
-        linkIcon.src = baseUrl + '/assets/images/blacktocat.png'; // Update the path to your custom PNG icon
-        linkIcon.alt = ''; // Add alt text for accessibility if needed
+        linkIcon.src = '{{ site.baseurl }}/assets/images/blacktocat.png'; // Update the path to your custom PNG icon
+        linkIcon.alt = 'linkme'; // Add alt text for accessibility if needed
         linkIcon.classList.add('anchor-link-icon');
-        linkIcon.style.marginRight = '5px'; // Add 5px margin to the right for spacing
-        heading.appendChild(linkIcon);
+        
+        // Append the icon to the container
+        iconContainer.appendChild(linkIcon);
+        
+        // Insert the container before the heading
+        heading.parentNode.insertBefore(iconContainer, heading);
+        
+        // Add margin to the right of the icon for spacing
+        iconContainer.style.marginRight = '5px';
+        
+        // Add margin to the right of the icon for spacing
+        iconContainer.style.marginRight = '5px';
+        
+        // Add a click event listener to the container to scroll to the heading
+        iconContainer.addEventListener('click', function() {
+            // Get the ID of the heading
+            const headingId = heading.getAttribute('id');
+            // Scroll to the heading
+            document.getElementById(headingId).scrollIntoView();
+        });
     });
 });
 
