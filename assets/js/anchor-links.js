3c4818912ce40cf35938b69ec676e83c0f857996
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-04-30 11:44:10 -0400

    Update anchor-links.js

--- assets/js/anchor-links.js
+++ assets/js/anchor-links.js
@@ -1,31 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]').forEach(function(heading) {
-        // Create a span element to contain the icon
-        const iconContainer = document.createElement('span');
-        iconContainer.classList.add('anchor-link-container');
-        
-        // Create the link icon
-        const linkIcon = document.createElement('img');
-        linkIcon.src = '{{ site.baseurl }}/assets/images/blacktocat.png'; // Update the path to your custom PNG icon
-        linkIcon.alt = ''; // Add alt text for accessibility if needed
+        const linkIcon = document.createElement('a');
+        linkIcon.href = '#' + heading.id;
+        linkIcon.innerHTML = '<svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M6.364 8.364a.5.5 0 0 1 0 .707l-2.5 2.5a.5.5 0 0 1-.708-.708l2.147-2.147-2.147-2.147a.5.5 0 0 1 .708-.708l2.5 2.5zM9.5 7.5h-8a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5zM8 11H3v-1h5v1zm0-2H3V8h5v1zm0-2H3V6h5v1zm0-2H3V4h5v1z"></path></svg>';
         linkIcon.classList.add('anchor-link-icon');
-        
-        // Append the icon to the container
-        iconContainer.appendChild(linkIcon);
-        
-        // Insert the container before the heading text
-        heading.parentNode.insertBefore(iconContainer, heading);
-        
-        // Add margin to the right of the icon for spacing
-        iconContainer.style.marginRight = '5px';
-        iconContainer.style.display = 'inline-block'; // Ensure the container and icon appear on the same line
-        
-        // Add a click event listener to the container to scroll to the heading
-        iconContainer.addEventListener('click', function() {
-            // Get the ID of the heading
-            const headingId = heading.getAttribute('id');
-            // Scroll to the heading
-            document.getElementById(headingId).scrollIntoView();
-        });
+        heading.appendChild(linkIcon);
     });
 });
+
