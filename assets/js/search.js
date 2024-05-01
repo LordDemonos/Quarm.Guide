6bc874336d518c53168d125d15bb5c837e1dbc4f
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-04-30 22:39:45 -0400

    Add files via upload

---
+++ assets/js/search.js
@@ -0,0 +1,7 @@
+// assets/js/search.js
+
+document.getElementById('search-input').addEventListener('input', function() {
+  var query = this.value.trim().toLowerCase();
+  // Perform search operation and filter content based on the query
+  // Example: filterContent(query);
+});
