45b2b69ef84e5e13f3db60b6fcfa2a5105750b9b
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-05-01 14:15:31 -0400

    Update mousewheel.js

--- assets/js/mousewheel.js
+++ assets/js/mousewheel.js
@@ -3,7 +3,7 @@
 document.addEventListener('mousewheel', function(event) {
   // Adjust these values to control the scrolling speed and smoothness
   const deltaY = event.deltaY * 0.5; // Adjust the scroll speed
-  const smooth = 0.2; // Adjust the smoothing factor (between 0 and 1)
+  const smooth = 0.5; // Adjust the smoothing factor (between 0 and 1)
 
   event.currentTarget.scrollBy({
     left: 0,
