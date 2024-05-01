cf5146f5b3dabf3739c820922eb2741b2a87d8ce
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-05-01 14:29:50 -0400

    Update mousewheel.js

--- assets/js/mousewheel.js
+++ assets/js/mousewheel.js
@@ -1,13 +1,14 @@
-// assets/js/mousewheel.js
+
+ // assets/js/mousewheel.js
 
 document.addEventListener('mousewheel', function(event) {
   // Adjust these values to control the scrolling speed and smoothness
-  const deltaY = event.deltaY * 0.3; // Adjust the scrolling speed by multiplying the change in vertical scroll (deltaY) by a factor of 0.3. 
-// Increase the multiplier (e.g., 0.5 or higher) to make scrolling slower, providing a smoother and more controlled scrolling experience for users.
-// Decrease the multiplier (e.g., 0.1 or lower) to make scrolling faster, allowing users to navigate through content more quickly but potentially sacrificing smoothness.
+  // Increase the multiplier (e.g., 0.5 or higher) to make scrolling slower, providing a smoother and more controlled scrolling experience for users.
+  // Decrease the multiplier (e.g., 0.1 or lower) to make scrolling faster, allowing users to navigate through content more quickly but potentially sacrificing smoothness.
+  const deltaY = event.deltaY * 0.3; // Adjust the scroll speed
   const smooth = 0.05; // Lower value for smoother scrolling
 
-  event.currentTarget.scrollBy({
+  window.scrollBy({
     left: 0,
     top: deltaY * smooth,
     behavior: 'auto'
