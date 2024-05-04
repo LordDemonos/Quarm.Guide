ae050d5020063aaf8cb9f79e6f37f98dbcfcc3ca
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-05-04 00:55:47 -0400

    Delete assets/js/git-wiki-custom.js

--- assets/js/git-wiki-custom.js
+++
@@ -1,7 +0,0 @@
-// Remove the color inversion filter from the body in dark mode
-document.addEventListener('DOMContentLoaded', function() {
-  var body = document.querySelector('body.darkmode--activated');
-  if (body) {
-    body.style.filter = 'none';
-  }
-});
