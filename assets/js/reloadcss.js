89ec1f575f4a5080568c72460f27e2e020e6788d
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-05-12 20:08:45 -0400

    reload

---
+++ assets/js/reloadcss.js
@@ -0,0 +1,8 @@
+function reloadCSS() {
+  var cssLink = document.getElementById('main-css');
+  if (cssLink) {
+    var href = cssLink.getAttribute('href').split('?')[0];  // Remove any query string
+    var newHref = href + '?v=' + new Date().getTime();  // Append timestamp to force reload
+    cssLink.setAttribute('href', newHref);
+  }
+}
