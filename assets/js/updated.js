cd811971ad87190e7d6f1c14589b539ff3da4c0c
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-05-12 20:20:41 -0400

    Cleanup

--- assets/js/updated.js
+++ assets/js/updated.js
@@ -6,20 +6,16 @@ async function fetchLastCommitDate() {
     if (cachedData) {
         const { lastFetched, date } = JSON.parse(cachedData);
         const hoursSinceLastFetch = (new Date() - new Date(lastFetched)) / (1000 * 60 * 60);
-        console.log("Cached data found, hours since last fetch:", hoursSinceLastFetch);
 
         if (hoursSinceLastFetch < 24) {
-            console.log("Using cached date:", new Date(date).toLocaleDateString());
             return new Date(date).toLocaleDateString(); // Return cached date if less than 24 hours have passed
         }
     }
 
-    console.log("Fetching new commit data from GitHub...");
     // Fetch new data if not cached or cache is older than 24 hours
     const response = await fetch(`https://api.github.com/repos/${repo}/commits`);
     const commits = await response.json();
     const lastCommitDate = new Date(commits[0].commit.committer.date);
-    console.log("New commit date fetched:", lastCommitDate);
 
     // Update cache
     localStorage.setItem(cacheKey, JSON.stringify({
