34565ef2ac9a6cf6fb48a37bd5df90f99bcd3c6e
LordDemonos <81699643+LordDemonos@users.noreply.github.com>
2024-06-20 09:25:21 -0400

    item icons

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,44 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const tooltipContainer = document.createElement('div');
-  tooltipContainer.id = 'tooltip-container';
-  document.body.appendChild(tooltipContainer);
-
-  let hideTooltipTimeout;
-
   document.querySelectorAll('a[href*="pqdi.cc/item/"]').forEach((link) => {
     const urlParts = link.href.split('/');
     const itemId = urlParts[urlParts.length - 1];
     link.classList.add('tooltip-link');
     link.setAttribute('data-item-id', itemId);
 
-    const iconSpan = document.createElement('span');
-    iconSpan.className = 'item-icon';
-    iconSpan.style.display = 'inline-block';
-    iconSpan.style.height = '1em';
-    iconSpan.style.width = '1em';
-    iconSpan.style.backgroundImage = `url(https://www.pqdi.cc/static/icons/item_${itemId}.png)`;
-    iconSpan.style.backgroundPosition = 'center center';
-    iconSpan.style.backgroundSize = 'cover';
-    link.parentNode.insertBefore(iconSpan, link);
-
-    link.addEventListener('mouseenter', function (event) {
-      clearTimeout(hideTooltipTimeout);
-      const cursorX = event.clientX;
-      const cursorY = event.clientY;
-      fetch(`https://www.pqdi.cc/get-item-tooltip/${itemId}`)
-        .then((response) => response.text())
-        .then((html) => {
-          tooltipContainer.innerHTML = html;
-          tooltipContainer.style.left = `${cursorX + 5}px`;
-          tooltipContainer.style.top = `${cursorY + 5}px`;
-          tooltipContainer.style.display = 'block';
-        });
-    });
-
-    link.addEventListener('mouseleave', function () {
-      hideTooltipTimeout = setTimeout(() => {
-        tooltipContainer.style.display = 'none';
-      }, 300);
-    });
+    fetch(`https://www.pqdi.cc/get-item-tooltip/${itemId}`)
+      .then((response) => response.text())
+      .then((html) => {
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = html;
+        const iconSpan = tempDiv.querySelector('.item-icon');
+        if (iconSpan) {
+          iconSpan.style.height = '1em';
+          iconSpan.style.width = '1em';
+          link.parentNode.insertBefore(iconSpan, link);
+        }
+      });
   });
 });
